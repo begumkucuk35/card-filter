b777b8eb3bf9170e514ae5c64434c139f3791971
begumkucuk35 <35begumkucuk@gmail.com>
2025-03-29 20:27:02 +0300

    Filter tab section added

--- src/components/FilterTab.jsx
+++ src/components/FilterTab.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { category, person } from "../constants/data";
 
 const FilterTab = () => {
-  return (
-    <div>FilterTab</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
+  const [personCategory, setPersonCategory] = useState([]);
 
-export default FilterTab
+  useEffect(() => {
+    setCategories(category[0].categories);
+    setPersonCategory(person[0].categories);
+  }, []);
+  return (
+    <div className="w-full px-5 sm:px-10 md:px-20 py-5">
+      <div className="my-2">
+        <h1 className="text-lg font-semibold my-2 cursor-pointer hover:underline">{person[0].title}</h1>
+        <ul className="flex flex-wrap">
+          {personCategory.map((person, index) => (
+            <li
+              key={index}
+              className="px-3 py-1 border-1 border-solid rounded-full me-2 my-1 border-[#2E82D0] cursor-pointer hover:shadow-md hover:shadow-[#2E82D0]/25"
+            >
+              <p className="text-[#4B4B4B] font-semibold text-sm">
+                {person.person_category}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="my-2">
+        <h1 className="text-lg font-semibold my-2 cursor-pointer hover:underline">{category[0].title}</h1>
+        <ul className="flex flex-wrap">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className="px-3 py-1 border-1 border-solid rounded-full me-2 border-[#2E82D0] cursor-pointer hover:shadow-md hover:shadow-[#2E82D0]/25"
+            >
+              <p className="text-[#4B4B4B] font-semibold text-sm">
+                {category.category_name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default FilterTab;
---
+++ src/constants/data.js
@@ -0,0 +1,59 @@
+export const category = [
+  {
+    title: "Categories",
+    categories: [
+      {
+        category_name: "Social Assistance / Services",
+      },
+      {
+        category_name: "Culture & Arts",
+      },
+      {
+        category_name: "E - Municipality",
+      },
+      {
+        category_name: "Transportation",
+      },
+      {
+        category_name: "Infrastructure",
+      },
+      {
+        category_name: "Environment/Urban Planning",
+      },
+      {
+        category_name: "Newest Services",
+      },
+    ],
+  },
+];
+export const person = [
+  {
+    title: "How do you define yourself?",
+    categories: [
+      {
+        person_category: "Student",
+      },
+      {
+        person_category: "Retiree/Senior",
+      },
+      {
+        person_category: "Mother/Woman",
+      },
+      {
+        person_category: "Sports Enthusiast",
+      },
+      {
+        person_category: "Disaster Victim",
+      },
+      {
+        person_category: "Veteran/Relative of Martyr",
+      },
+      {
+        person_category: "Tourist",
+      },
+      {
+        person_category: "Disabled",
+      },
+    ],
+  },
+];
